3ec7323373851615f23f74cb487535bd741d756c
gks <gksk@users.noreply.github.com>
2022-05-27 23:58:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds(
 
 var dateTime = date+' '+time;
 
-<<<<<<< HEAD
-console.log('Success Demo @ '+dateTime)git add .
-=======
+
 console.log('Success Demo @ '+dateTime)
->>>>>>> c1748029e8dedae52e7d30352f285db6ec3e1707
+
