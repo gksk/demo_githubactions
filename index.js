2e829a2e3fb24435e3ac71b41aaf66b2bb72f000
gks <gksk@users.noreply.github.com>
2022-05-27 22:03:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+
+var today = new Date();
+
+var date = today.getFullYear()+'/'+(today.getMonth()+1)+'/'+today.getDate();
+
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+var dateTime = date+' '+time;
+
+<<<<<<< HEAD
+console.log('Success Demo @ '+dateTime)git add .
+=======
+console.log('Success Demo @ '+dateTime)
+>>>>>>> c1748029e8dedae52e7d30352f285db6ec3e1707
